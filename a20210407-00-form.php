1c1b9d8aecd5935c4a85ca3d410b9a3abd85a257
yuchen <p33440761@gmail.com>
2021-04-07 12:04:06 +0800

    php0407

---
+++ a20210407-00-form.php
@@ -0,0 +1,39 @@
+<?php include __DIR__ . '/parts/config.php'; ?>
+<?php include __DIR__ . '/parts/html-head.php'; ?>
+<?php include __DIR__ . '/parts/navbar.php'; ?>
+<div class="container">
+    <div class="row">
+        <pre>
+            <?php print_r($_POST)  ?> 
+        </pre>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <form method="post" action="">
+
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <div class="form-group">
+                    <label for="mydate">Date</label>
+                    <input type="datetime-local" class="form-control" id="mydate" name="mydate">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" name="check1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/parts/scripts.php'; ?>
+<?php include __DIR__ . '/parts/html-foot.php'; ?>
---
+++ test.sql
@@ -0,0 +1,2 @@
+INSERT INTO `address_book` (`sid`, `name`, `email`, `mobile`, `birthday`, `address`, `created_at`) 
+VALUES (NULL, '李曉明10', 'ming@gmail.com', '0988888555', '2013-04-11', '台北市', NOW());
